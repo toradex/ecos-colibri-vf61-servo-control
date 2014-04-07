326082a64a8fc157831c12a8f0bc04f94583d7f2
Michael Gielda <mgielda@antmicro.com>
2014-04-07 11:30:07 +0200

    Nomenclature normalization

--- src/make.sh
+++ src/make.sh
@@ -6,12 +6,12 @@ export PATH="{path/to/toolchain}/bin:$PATH"   # modify this
 # Set compiler options
 OPT="-Wall -Wpointer-arith -Wstrict-prototypes -Wundef -Wno-write-strings -mthumb -g -O2 -ffunction-sections -fdata-sections -fno-exceptions -nostdlib -mcpu=cortex-m4"
 
-# Set path to eCos kernel
-KPATH="ecos-kernel"
+# Set path to eCos build tree
+BTPATH="build-tree"
 
 # Do compilation and link your application with kernel.
-arm-eabi-gcc -g -I./ -g -I${KPATH}/install/include plotter.c \
--L${KPATH}/install/lib -Ttarget.ld ${OPT}
+arm-eabi-gcc -g -I./ -g -I${BTPATH}/install/include plotter.c \
+-L${BTPATH}/install/lib -Ttarget.ld ${OPT}
 
 # Copy to binary file
 arm-eabi-objcopy -O binary a.out plotter.bin
